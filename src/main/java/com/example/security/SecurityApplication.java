aee14aafe079d26a326d9488563db5196646b88e
wheejuni <wheejuni@gmail.com>
2017-11-27 12:11:24 +0900

    login with basic in-memory and auto-gen form

--- src/main/java/com/example/security/SecurityApplication.java
+++ src/main/java/com/example/security/SecurityApplication.java
@@ -2,11 +2,24 @@ package com.example.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
+
+	@GetMapping("/")
+    public String hello() {
+	    return "fuck you!";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+	    return "this is admin page.";
+    }
 }
